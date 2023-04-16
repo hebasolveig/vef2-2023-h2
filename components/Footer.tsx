019be716a02ef64f4ba72661184f141009d90995
hebasolveig <hsh58@hi.is>
2023-04-16 17:49:56 +0000

    -laga texta

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white flex justify-center items-center h-16 text-lg mt-8">
-      <p>Geggjuð videosíða</p>
+      <p>© 2023 Myndbandavefurinn</p>
     </footer>
   );
 };
